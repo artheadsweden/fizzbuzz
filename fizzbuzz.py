5151b97d335064e259c0b8a6b9a930537e9d40a3
Joakim Wassberg <joakim@arthead.se>
2020-09-14 13:30:00 +0200

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def main():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        if i % 3 != 0 and i % 5 != 0:
+            print(i, end="")
+        print()
+
+
+if __name__ == '__main__':
+    main()
