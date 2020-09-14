854d6f6c605c04425b56c7241d952f5e188be629
Joakim Wassberg <joakim@arthead.se>
2020-09-14 14:05:14 +0200

    Solved answer for 42

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,13 +2,16 @@
 # 2
 def main():
     for i in range(1, 101):
-        if i % 3 == 0:
-            print("Fizz", end="")
-        if i % 5 == 0:
-            print("Buzz", end="")
-        if i % 3 != 0 and i % 5 != 0:
-            print(i, end="")
-        print()
+        if i == 42:
+            print("Answer to the Ultimate Question of Life, the Universe, and Everything")
+        else:
+            if i % 3 == 0:
+                print("Fizz", end="")
+            if i % 5 == 0:
+                print("Buzz", end="")
+            if i % 3 != 0 and i % 5 != 0:
+                print(i, end="")
+            print()
 
 
 if __name__ == '__main__':
